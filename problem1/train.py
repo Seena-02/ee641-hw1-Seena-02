13bb22102a5443516a89b92abc085e32a3981674
Seena Mohajeran <seenamohajeran@gmail.com>
2025-09-15 16:09:09 -0700

    check

--- problem1/train.py
+++ problem1/train.py
@@ -81,7 +81,7 @@ def main():
     # Configuration
     batch_size = 16
     learning_rate = 0.001
-    num_epochs = 5
+    num_epochs = 50
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
